2cc388ecdf14041e32aba631c520544860c77dd8
TanguyBarthelemy <--local>
2025-04-01 18:33:20 +0200

    typo and add coverage

--- R/format.R
+++ R/format.R
@@ -4,6 +4,6 @@ format_timestamp <- function(x) {
     output <- x |>
         as.POSIXct(origin = 0L) |> #"1970-01-01") |>
         as.integer() |>
-        as.POSIXct(origin = 0L) |> #"1970-01-01")
+        as.POSIXct(origin = 0L) #"1970-01-01")
     return(output)
 }
