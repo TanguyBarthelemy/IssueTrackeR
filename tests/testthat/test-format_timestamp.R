5eef820776607aaa9c62e43fd51490661cce8710
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2025-04-24 12:09:09 +0200

    update tests for version inferior as 4.2

--- tests/testthat/test-format_timestamp.R
+++ tests/testthat/test-format_timestamp.R
@@ -1,6 +1,7 @@
 test_that("format_timestamp works", {
 
-    output_expected <- as.POSIXct(1743694674.0)
+    output_expected <- structure(1743694674.0, class = c("POSIXct", "POSIXt"),
+                                 tzone = "")
     testthat::expect_identical(
         object = IssueTrackeR:::format_timestamp(1743694674.9),
         expected = output_expected
