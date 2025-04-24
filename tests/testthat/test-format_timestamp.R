17046715a54d653f5ca0e5ccceb5bb6b8c151968
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2025-04-24 13:07:21 +0200

    remove structure

--- tests/testthat/test-format_timestamp.R
+++ tests/testthat/test-format_timestamp.R
@@ -1,7 +1,9 @@
 test_that("format_timestamp works", {
 
-    output_expected <- structure(1743694674.0, class = c("POSIXct", "POSIXt"),
-                                 tzone = "")
+    output_expected <- 1743694674.0
+    class(output_expected) <- c("POSIXct", "POSIXt")
+    attr(x = output_expected, which = "tzone") <- ""
+
     testthat::expect_identical(
         object = IssueTrackeR:::format_timestamp(1743694674.9),
         expected = output_expected
