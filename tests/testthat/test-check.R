3435a39549fd965215b048b8dc6500c14a8aa423
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-06-21 15:18:37 +0000

    update check with separating object and check

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,6 +1,5 @@
 test_that("Checks for fail API call", {
-    expect_error({
-        check_response(try(
+        x1 <- try(
             {
                 gh::gh(
                     repo = "aa",
@@ -12,72 +11,65 @@ test_that("Checks for fail API call", {
                 )
             },
             silent = TRUE
-        ))
-    })
+        )
+    x2 <- try(
+        {
+            gh::gh(
+                endpoint = "/users/:owner",
+                owner = "Tanguyyyyyyyy",
+                .limit = Inf,
+                .progress = FALSE
+            )
+        },
+        silent = TRUE
+    )
+    x3 <- try(
+        {
+            gh::gh(
+                repo = "aa",
+                owner = "bb",
+                endpoint = "/repos/:owner/:repo/milestones",
+                state = "all",
+                .limit = Inf,
+                .progress = FALSE
+            )
+        },
+        silent = TRUE
+    )
+    x4 <- try(
+        {
+            gh::gh(
+                repo = "aa",
+                owner = "bb",
+                endpoint = "/repos/:owner/:repo/labels",
+                .limit = Inf,
+                .progress = FALSE
+            )
+        },
+        silent = TRUE
+    )
+    x5 <- try(
+        {
+            gh::gh(
+                endpoint = "/orgs/:owner/repos",
+                owner = "Tanguyyyyyyyy",
+                .limit = Inf,
+                .progress = FALSE
+            )
+        },
+        silent = TRUE
+    )
 
-    expect_error({
-        check_response(try(
-            {
-                gh::gh(
-                    endpoint = "/users/:owner",
-                    owner = "Tanguyyyyyyyy",
-                    .limit = Inf,
-                    .progress = FALSE
-                )
-            },
-            silent = TRUE
-        ))
-    })
-
-    expect_error({
-        check_response(try(
-            {
-                gh::gh(
-                    repo = "aa",
-                    owner = "bb",
-                    endpoint = "/repos/:owner/:repo/milestones",
-                    state = "all",
-                    .limit = Inf,
-                    .progress = FALSE
-                )
-            },
-            silent = TRUE
-        ))
-    })
-
-    expect_error({
-        check_response(try(
-            {
-                gh::gh(
-                    repo = "aa",
-                    owner = "bb",
-                    endpoint = "/repos/:owner/:repo/labels",
-                    .limit = Inf,
-                    .progress = FALSE
-                )
-            },
-            silent = TRUE
-        ))
-    })
-
-    expect_error({
-        check_response(try(
-            {
-                gh::gh(
-                    endpoint = "/orgs/:owner/repos",
-                    owner = "Tanguyyyyyyyy",
-                    .limit = Inf,
-                    .progress = FALSE
-                )
-            },
-            silent = TRUE
-        ))
-    })
+    expect_error(check_response(x1))
+    expect_error(check_response(x2))
+    expect_error(check_response(x3))
+    expect_error(check_response(x4))
+    expect_error(check_response(x5))
 })
 
 test_that("Checks for good call", {
-    expect_null(
-        object = check_response(
+    x6 <- try(
+        {
             gh::gh(
                 repo = "IssueTrackeR",
                 owner = "TanguyBarthelemy",
@@ -86,8 +78,11 @@ test_that("Checks for good call", {
                 .limit = Inf,
                 .progress = FALSE
             )
-        )
+        },
+        silent = TRUE
     )
+
+    expect_null(check_response(x6))
 })
 
 
